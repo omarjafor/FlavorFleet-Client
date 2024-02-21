7c340ed270fc08cd9e188f06a98c472331a63d77
omarjafor <omarjaforchy@gmail.com>
2024-02-21 23:51:52 +0600

    redux and cart slice store

--- src/Components/FoodCard.jsx
+++ src/Components/FoodCard.jsx
@@ -1,6 +1,9 @@
 import { AiFillStar } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/slice/CartSlice";
 
 const FoodCard = ({ id, name, price, desc, img, rating, handleToast }) => {
+    const dispatch = useDispatch();
     
     return (
         <div className="font-bold w-[250px] bg-white p-5 flex flex-col rounded-lg gap-2">
@@ -9,12 +12,17 @@ const FoodCard = ({ id, name, price, desc, img, rating, handleToast }) => {
                 <h2 className="">{name}</h2>
                 <span className="text-green-400">${price}</span>
             </div>
-            <p className="text-sm font-normal">{desc.slice(0, 50)}...</p>
+            <p className="text-sm font-normal">{desc.slice(0, 40)}...</p>
             <div className="flex justify-between">
                 <span className="flex justify-center items-center">
                     <AiFillStar className="mr-1 text-yellow-400" /> {rating}
                 </span>
-                <button className="p-1 text-white bg-green-500 hover:bg-green-600 rounded-lg text-sm">
+                <button onClick={() => {
+                    dispatch(
+                        addToCart({ id, name, price, rating, img, qty: 1 })
+                    );
+                }}
+                className="p-1 text-white bg-green-500 hover:bg-green-600 rounded-lg text-sm">
                     Add to Cart
                 </button>
             </div>
