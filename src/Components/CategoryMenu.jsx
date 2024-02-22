d4730ce3311c78b1d5827993d6946c2f0edfcdb7
omarjafor <omarjaforchy@gmail.com>
2024-02-22 13:29:59 +0600

    category menu working

--- src/Components/CategoryMenu.jsx
+++ src/Components/CategoryMenu.jsx
@@ -1,15 +1,48 @@
-
+import { useEffect, useState } from "react";
+import FoodData from "../assets/FoodData";
+import { useDispatch, useSelector } from "react-redux";
 
 const CategoryMenu = () => {
+    const dispatch = useDispatch();
+    const selectedCategory = useSelector((state) => state.category.category);
+    const [categories, setCategories] = useState([]);
+
+    const listCategories = () => {
+        const allCategories = [
+            ...new Set(FoodData.map(food => food.category))
+        ];
+        setCategories(allCategories);
+    }
+
+    useEffect(() => {
+        listCategories();
+    }, []);
+    
     return (
         <div className='ml-6'>
             <h3 className='text-xl font-semibold'>Find The Best Food</h3>
             <div className='my-5 flex gap-3 overflow-x-scroll scroll-smooth lg:overflow-x-hidden'>
-                <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>All</button>
-                <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>Lunch</button>
-                <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>Breakfast</button>
-                <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>Dinner</button>
-                <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>Snacks</button>
+                <button
+                    onClick={() => dispatch(setCategory("All"))}
+                    className={`px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white ${selectedCategory === "All" && "bg-green-500 text-white"
+                        }`}
+                >
+                    All
+                </button>
+                {
+                    categories.map((category, index) => {
+                        return (
+                            <button
+                                onClick={() => dispatch(setCategory(category))}
+                                key={index}
+                                className={`px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white ${selectedCategory === category && "bg-green-500 text-white"
+                                    } `}
+                            >
+                                {category}
+                            </button>
+                        )
+                    })
+                }
             </div>
         </div>
     );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CartSlice from "./slice/CartSlice";
+import CategorySlice from "./slice/CategorySlice";
 
 const store = configureStore({
     reducer: {
         cart: CartSlice,
+        category: CategorySlice,
     }
 });
 
